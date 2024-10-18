048113aca457e649d05074b72c00e3ede62714ff
pagunduu <pagundu@outlook.com>
2024-10-18 21:06:01 +0530

    started configuring neovim

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,21 @@
+vim.opt.background       =   "dark"
+
+vim.g.mapleader         =   " "
+vim.g.have_nerd_font    =   true
+
+vim.opt.number 		    = 	true
+vim.opt.relativenumber	= 	true
+
+vim.opt.shiftwidth	    =	4
+vim.opt.tabstop		    =	4
+vim.opt.softtabstop	    =	4
+
+vim.opt.expandtab	    =	true
+vim.opt.autoindent	    =	true
+vim.opt.smartindent	    =	true
+
+vim.opt.ignorecase      =   true
+vim.opt.smartcase       =   true
+
+vim.opt.signcolumn      =   'yes'
+vim.opt.scrolloff       =   10
